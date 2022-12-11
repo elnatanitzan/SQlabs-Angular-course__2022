d9c19c0ecffc41ed374c83448bd255df6c5524ba
Elnatan Nitzan <e.nitzan@easysend.io>
2022-12-11 19:30:47 +0200

    Lesson 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { FormsModule } from '@angular/forms';
+// COMPONENTS REGION:
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { ButtonComponent } from './components/button/button.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +19,14 @@ import { ButtonComponent } from './components/button/button.component';
     FooterComponent,
     TasksComponent,
     TaskItemComponent,
-    ButtonComponent
+    ButtonComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,33 +1,32 @@
-import { Component } from '@angular/core';
-import { Task } from '../../../Task.interface';
-import { TasksService } from '../../services/tasks.service';
+import { Component } from "@angular/core";
+import { Task } from "../../../Task.interface";
+import { TasksService } from "../../services/tasks.service";
 
 @Component({
-  selector: 'app-tasks',
-  templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css'],
+	selector: "app-tasks",
+	templateUrl: "./tasks.component.html",
+	styleUrls: ["./tasks.component.css"],
 })
 export class TasksComponent {
-  constructor(private tasksService: TasksService) {}
-  tasks: Task[] = [];
+	constructor(private tasksService: TasksService) {}
+	tasks: Task[] = [];
 
-  ngOnInit(): void {
-    this.tasksService
-      .getTasks()
-      .subscribe((tasks: Task[]) => (this.tasks = tasks));
-  }
+	ngOnInit(): void {
+		this.tasksService
+			.getTasks()
+			.subscribe((tasks: Task[]) => (this.tasks = tasks));
+	}
 
-  onToggleReminder(e: MouseEvent, task: Task): void {
-    task.reminder = !task.reminder;
-    this.tasksService.toggleTaskReminder(task).subscribe();
-  }
+	onToggleReminder(e: MouseEvent, task: Task): void {
+		task.reminder = !task.reminder;
+		this.tasksService.toggleTaskReminder(task).subscribe();
+	}
 
-  deleteTask(task: Task): void {
-    this.tasksService
-      .deleteTask(task)
-      .subscribe(
-        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
-      );
-      
-  }
+	deleteTask(task: Task): void {
+		this.tasksService
+			.deleteTask(task)
+			.subscribe(
+				() => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+			);
+	}
 }
